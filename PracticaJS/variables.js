6f789af4657bb9370fbea1bfb33795462fc33056
ALANMBO <bejaranomauricio999@gmail.com>
2024-12-06 11:27:34 -0400

    archivo para ejercicios js

---
+++ PracticaJS/variables.js
@@ -0,0 +1,10 @@
+let numero = 10;
+let cadena = "hola";
+let booleano = true;
+let arreglo = [1, 2, 3];
+
+
+console.log("MI NUMERO ES "+numero);
+console.log("MI CADENA ES "+cadena);
+console.log("MI BOOLEANO ES "+booleano);
+console.log("MI ARREGLO ES "+arreglo);
